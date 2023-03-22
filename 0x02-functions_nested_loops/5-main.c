08afda3ba7d26781ad70f50e908d23b2db32b00e
Kessa23 <khavetsabertillah@gmail.com>
2023-03-22 04:31:49 -0700

    Sign

--- 0x02-functions_nested_loops/5-main.c
+++ 0x02-functions_nested_loops/5-main.c
@@ -1,9 +1,10 @@
+#include <limits.h>
 #include "main.h"
 
 int print_sign(int n);
 
 /**
- * main - check the code.
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
@@ -12,23 +13,8 @@ int main(void)
 	int r;
 
 	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
+	if (r < 0)
+		r *= -1;
 	_putchar(r + '0');
 	_putchar('\n');
 	return (0);
