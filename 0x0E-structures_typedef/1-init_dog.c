c0a1be48457f807d03aca7a8e4363b1047b56b05
Kessa23 <khavetsabertillah@gmail.com>
2023-04-17 10:34:25 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 
 /**
  * init_dog: initializes a dog structure
@@ -16,4 +17,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
+	}	
 }
